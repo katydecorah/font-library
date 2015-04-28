60621ede6cf6eb8d446c331e2ff0a5b470cdd5cd
Katy DeCorah <katydecorah@gmail.com>
2015-04-28 18:30:34 -0400

    cleans up tags, checks for variants, and subsets

--- js/app.js
+++ js/app.js
@@ -12,13 +12,13 @@ app.controller('ctrl', function($scope, $filter, $http, $location) {
   var locationSearch = path[1];
   var locationPage = path[2];
 
-  if(locationPage){
+  if (locationPage){
     $scope.currentPage = locationPage - 1;
-  }else {
+  } else {
     $scope.currentPage = 0;
   }
 
-  if(locationSearch) {
+  if (locationSearch) {
     $scope.search = locationSearch;
   } else {
     $scope.search = 'tattoo';
@@ -30,14 +30,24 @@ app.controller('ctrl', function($scope, $filter, $http, $location) {
   .then(function(res){
     $scope.data = res.data;
 
-    // log fonts that only have less than 1 tag
+    $scope.helpWantedTags();
+    $scope.helpWantedNewFont();
+  });
+
+  /* log fonts that only have less than 1 tag */
+  $scope.helpWantedTags = function() {
+    console.groupCollapsed("Help wanted! These fonts need more tags in families.json");
+    console.info("Using the provided link to the font's specimen, add tags to families.json for these font families.");
     angular.forEach($scope.data, function(key) {
       if (key.tags.length < 2) {
-        console.log("Needs more tags: " + key.family + "\thttps://www.google.com/fonts/specimen/"+key.family.split(" ").join("+"));
+        console.log(key.family + "\thttps://www.google.com/fonts/specimen/"+key.family.split(" ").join("+"));
       }
     });
+    console.groupEnd();
+  };
 
-    // log new fonts - fonts that needs to be added to families.json
+  /* log new fonts - fonts that needs to be added to families.json */
+  $scope.helpWantedNewFont = function() {
     var storedFamilies = [];
     var apiFamilies = [];
     var url = "https://www.googleapis.com/webfonts/v1/webfonts?key=AIzaSyDK4Jz71F7DQCrUhXYaF3xgEXoQGLDk5iE";
@@ -51,65 +61,98 @@ app.controller('ctrl', function($scope, $filter, $http, $location) {
       angular.forEach($scope.api, function(key) {
         apiFamilies.push(key.family);
       });
-      var needToAdd = _.difference(apiFamilies,storedFamilies);
+
+      /*
+      console.groupCollapsed("Not latin subset");
+      var found = $filter('filter')($scope.api, {subsets:'!latin'});
+      angular.forEach(found, function(key) {
+      console.log(key.family + ' ' + key.subsets);
+    });
+    console.groupEnd();
+    */
+
+    var needToAdd = _.difference(apiFamilies,storedFamilies);
+    if (needToAdd.length) {
+      console.groupCollapsed("Help wanted! These fonts need to be added to families.json");
+      console.info("Copy and paste the following into families.json, be sure to keep the families names in alphabetical order.");
       angular.forEach(needToAdd, function(key) {
         var found = $filter('filter')($scope.api, {family: key}, true);
-         if (found.length) {
-             var cat = found[0].category;
-         }
-        console.log('Need to add: { "family": "'+key+'", "tags": ["'+cat+'"] },\thttp://www.google.com/fonts#UsePlace:use/Collection:'+key.split(' ').join('+'));
+        if (found.length) {
+          var cat = found[0].category;
+        }
+        // check variants
+        if ( $.inArray('regular', found[0].variants) == -1 ) {
+
+          // if italic
+          if(  $.inArray('italic', found[0].variants) == 0 ) {
+            console.log('{ "family": "'+key+'", "tags": ["'+cat+'"], "italic": "400italic" },');
+          } else {
+            console.log('{ "family": "'+key+'", "tags": ["'+cat+'"], "weight": "'+found[0].variants[0]+'" },');
+          }
+
+        }
+        // check subsets
+        else if ( $.inArray('latin', found[0].subsets) == -1 ) {
+          console.log('{ "family": "'+key+'", "tags": ["'+cat+'","'+found[0].subsets+'"] },');
+        } else {
+          console.log('{ "family": "'+key+'", "tags": ["'+cat+'"] },');
+        }
+
       });
-    });
+      console.groupEnd();
+    }
   });
 
+};
 
-  $scope.updateLocation = function() {
-    $location.path("/"+$scope.search);
-  };
-  $scope.reset = function() {
-    $location.path("");
-    $scope.search = "";
-  };
 
-  $scope.updateLocPage = function() {
-    $location.path("/"+$scope.search+"/"+ ($scope.currentPage + 1 ) );
-    window.scrollTo(0, 0);
-  };
+$scope.updateLocation = function() {
+  $location.path("/"+$scope.search);
+};
+$scope.reset = function() {
+  $location.path("");
+  $scope.search = "";
+};
 
-  $scope.resetPagination = function() {
-    $scope.currentPage = 0;
-    $scope.updateLocation();
-  };
+$scope.updateLocPage = function() {
+  $location.path("/"+$scope.search+"/"+ ($scope.currentPage + 1 ) );
+  window.scrollTo(0, 0);
+};
 
-  $scope.selectTag = function(i) {
-    $scope.search = i;
-    $scope.resetPagination();
-    $location.path("/"+i);
-    window.scrollTo(0, 0);
-  };
+$scope.resetPagination = function() {
+  $scope.currentPage = 0;
+  $scope.updateLocation();
+};
 
-  $scope.numberOfPages=function(){
-    var myFilteredData = $filter('filter')($scope.data,$scope.search,true);
-    return Math.ceil(myFilteredData.length/$scope.pageSize);
-  }
+$scope.selectTag = function(i) {
+  $scope.search = i;
+  $scope.resetPagination();
+  $location.path("/"+i);
+  window.scrollTo(0, 0);
+};
 
-  $scope.numberOfResults=function(){
-    var myFilteredData = $filter('filter')($scope.data,$scope.search,true);
-    return Math.ceil(myFilteredData.length);
-  }
+$scope.numberOfPages=function(){
+  var myFilteredData = $filter('filter')($scope.data,$scope.search,true);
+  return Math.ceil(myFilteredData.length/$scope.pageSize);
+}
 
-  $scope.uniqueTags = function() {
-    return _.chain($scope.data)
-    .pluck('tags')
-    .flatten()
-    .countBy()
-    .value();
-  };
+$scope.numberOfResults=function(){
+  var myFilteredData = $filter('filter')($scope.data,$scope.search,true);
+  return Math.ceil(myFilteredData.length);
+}
+
+$scope.uniqueTags = function() {
+  return _.chain($scope.data)
+  .pluck('tags')
+  .flatten()
+  .countBy()
+  .value();
+};
 })
 
 app.filter('startFrom', function() {
   return function(input, start) {
-    start = +start; //parse to int
+    start = +start;
     return input.slice(start);
   }
 });
