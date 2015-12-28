d75ce213bdb5b68d5392c1c5b58519bcc94671d1
Katy DeCorah <katydecorah@gmail.com>
2015-12-27 21:55:00 -0500

    check if font exists

--- test/families.test.js
+++ test/families.test.js
@@ -2,32 +2,54 @@ var test = require('tape');
 var fs = require('fs');
 var path =  require('path');
 var jsyaml = require('js-yaml');
+var request = require('request');
 
 var path = 'families.json';
 
 var families = JSON.parse(fs.readFileSync(path));
+var url = 'https://www.googleapis.com/webfonts/v1/webfonts?key=AIzaSyDK4Jz71F7DQCrUhXYaF3xgEXoQGLDk5iE';
 
-families.reduce(function(prev, post, currentIndex, array) {
-  test(post.family, function(t) {
-    t.equal( typeof post, 'object', "family must be formatted correctly");
-    t.ok(post.family, "must have a family");
-    t.ok(post.tags, "must have tags");
-    // no more than 5 tags
-    if (post.tags) {
-      t.equal(post.tags.length < 6, true, 'no more than 5 tags');
-    }
-    // tags must be lowercase
-    post.tags.forEach(function(tag){
-      if (isNaN(tag[0]) && tag[0] == tag[0].toUpperCase()) {
-        t.fail(tag + " tag must be lowercase")
+request(url, onRequestDone);
+
+function onRequestDone(err, resp, body) {
+  var googleFamilies = [];
+  if (!err && resp.statusCode == 200) {
+    var json = JSON.parse(body);
+    json.items.forEach(function(i){
+      googleFamilies.push(i.family);
+    })
+  }
+  runTests(googleFamilies);
+}
+
+function runTests(googleFamilies) {
+  
+  families.reduce(function(prev, post, currentIndex, array) {
+    test(post.family, function(t) {
+      t.equal( typeof post, 'object', "family must be formatted correctly");
+      t.ok(post.family, "must have a family");
+      t.ok(post.tags, "must have tags");
+      
+      // check if font exists in Google Fonts
+      t.notEqual(googleFamilies.indexOf(post.family),-1, "The font '"+ post.family +"' does not match a font found in Google Fonts");
+      
+      // no more than 5 tags
+      if (post.tags) {
+        t.equal(post.tags.length < 6, true, "no more than 5 tags");
       }
+      // tags must be lowercase
+      post.tags.forEach(function(tag){
+        if (isNaN(tag[0]) && tag[0] == tag[0].toUpperCase()) {
+          t.fail(tag + " tag must be lowercase")
+        }
+      });
+      // make sure families are in alphabetical order
+      var prevFamily = families[currentIndex - 1].family;
+      if ( prevFamily > post.family ) {
+        t.fail("Font families must be in alphabetical order: '" + post.family + "' should appear before '" + prevFamily + "'");
+      }    
+      
+      t.end();
     });
-    // make sure families are in alphabetical order
-    var prevFamily = families[currentIndex - 1].family;
-    if ( prevFamily > post.family ) {
-      t.fail("Font families must be in alphabetical order: '" + post.family + "' should appear before '" + prevFamily + "'");
-    }    
-    
-    t.end();
-  });
-});
+  });  
+}
