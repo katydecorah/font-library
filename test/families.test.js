735e63a17be93fd6ae3d6d5b3c340825adc484e8
Katy DeCorah <katydecorah@gmail.com>
2022-02-26 14:43:24 -0500

    Switch to Jest

--- test/families.test.js
+++ test/families.test.js
@@ -1,69 +1,49 @@
-const test = require("tape");
 const fetch = require("node-fetch");
 const families = require("../families.json");
 
 // build list of family names in families.json
 const familiesList = families.map(({ family }) => family);
+let googleFamilies = [];
 
-(async () => {
-  try {
-    const response = await fetch(
-      "https://www.googleapis.com/webfonts/v1/webfonts?key=AIzaSyDK4Jz71F7DQCrUhXYaF3xgEXoQGLDk5iE"
-    );
-    const { items } = await response.json();
-    // build list of family names in Google Fonts API
-    const googleFamilies = items.map(({ family }) => family);
-    runTests(googleFamilies);
-  } catch (err) {
-    console.log(err);
-  }
-})();
+beforeAll(async () => {
+  const response = await fetch(
+    "https://www.googleapis.com/webfonts/v1/webfonts?key=AIzaSyDK4Jz71F7DQCrUhXYaF3xgEXoQGLDk5iE"
+  );
+  const { items } = await response.json();
+  // build list of family names in Google Fonts API
+  googleFamilies = items.map(({ family }) => family);
+});
 
-function runTests(googleFamilies) {
-  // test each family in families.json
-  families.forEach(({ family, tags }, index) => {
-    test(family, (t) => {
-      t.ok(family, "must have a family");
-      t.ok(tags, "must have tags");
+// test each family in families.json
+families.forEach(({ family, tags }, index) => {
+  test(family, async () => {
+    expect(family).toBeDefined();
+    expect(tags).toBeDefined();
 
-      // check if font exists in Google Fonts
-      t.notEqual(
-        googleFamilies.indexOf(family),
-        "-1",
-        `The font '${family}' does not match a font found in Google Fonts`
-      );
+    // check if font exists in Google Fonts
+    expect(googleFamilies.includes(family)).toBeTruthy();
 
-      // no more than 5 tags
-      if (tags) {
-        t.equal(tags.length < 6, true, "no more than 5 tags");
-      }
-      // tags must be lowercase
-      tags.forEach((tag) => {
-        if (isNaN(tag[0]) && tag[0] == tag[0].toUpperCase()) {
-          t.fail(`${tag} tag must be lowercase`);
-        }
-      });
-      // make sure families are in alphabetical order
-      const prevFamily = families[index - 1]
-        ? families[index - 1].family
-        : undefined;
-      if (prevFamily > family) {
-        t.fail(
-          `Font families must be in alphabetical order: '${family}' should appear before '${prevFamily}'`
-        );
-      }
-
-      t.end();
-    });
-  });
-
-  // check Google Fonts API for new fonts
-  for (const font of googleFamilies) {
-    if (!familiesList.includes(font)) {
-      test(font, (t) => {
-        t.fail(`Add the new font '${font}' to families.json`);
-        t.end();
-      });
+    // no more than 5 tags
+    if (tags) {
+      expect(tags.length < 6).toBeTruthy();
     }
+    // tags must be lowercase
+    tags.forEach((tag) => {
+      expect(isNaN(tag[0]) && tag[0] == tag[0].toUpperCase()).toBeFalsy();
+    });
+    // make sure families are in alphabetical order
+    const prevFamily = families[index - 1]
+      ? families[index - 1].family
+      : undefined;
+    expect(prevFamily > family).toBeFalsy();
+  });
+});
+
+// check Google Fonts API for new fonts
+for (const font of googleFamilies) {
+  if (!familiesList.includes(font)) {
+    test(`Add new font: ${font}`, () => {
+      expect(font).toBeDefined();
+    });
   }
 }
