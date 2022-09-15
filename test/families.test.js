163e6601def2b4b6ece7545a8e02574744059393
Katy DeCorah <katydecorah@gmail.com>
2022-09-14 21:05:09 -0400

    Clean up tests, run action on pull_request

--- test/families.test.js
+++ test/families.test.js
@@ -1,27 +1,10 @@
-const fetch = require("node-fetch");
 const families = require("../families.json");
-
-// build list of family names in families.json
-const familiesList = families.map(({ family }) => family);
-let googleFamilies = [];
-
-beforeAll(async () => {
-  const response = await fetch(
-    "https://www.googleapis.com/webfonts/v1/webfonts?key=AIzaSyDK4Jz71F7DQCrUhXYaF3xgEXoQGLDk5iE"
-  );
-  const { items } = await response.json();
-  // build list of family names in Google Fonts API
-  googleFamilies = items.map(({ family }) => family);
-});
 
 // test each family in families.json
 families.forEach(({ family, tags }, index) => {
   test(family, async () => {
     expect(family).toBeDefined();
     expect(tags).toBeDefined();
-
-    // check if font exists in Google Fonts
-    expect(googleFamilies.includes(family)).toBeTruthy();
 
     // no more than 5 tags
     if (tags) {
@@ -38,12 +21,3 @@ families.forEach(({ family, tags }, index) => {
     expect(prevFamily > family).toBeFalsy();
   });
 });
-
-// check Google Fonts API for new fonts
-for (const font of googleFamilies) {
-  if (!familiesList.includes(font)) {
-    test(`Add new font: ${font}`, () => {
-      expect(font).toBeDefined();
-    });
-  }
-}
