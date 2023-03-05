58e80d589296d9ff9d5d3d3b2c58e99955c820d7
Katy DeCorah <katydecorah@gmail.com>
2023-03-04 20:01:15 -0500

    Update fonts.js

--- js/fonts.js
+++ js/fonts.js
@@ -1,6 +1,6 @@
 // https://www.raymondcamden.com/2022/05/23/building-table-sorting-and-pagination-in-a-web-component
 
-const svgClose = `<svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>`;
+// const svgClose = `<svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>`;
 
 class FontResults extends HTMLElement {
   constructor() {
