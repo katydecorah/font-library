55cb7147da6b5bb39896f5ce7c19ac6982a8b8e1
Katy DeCorah <katydecorah@gmail.com>
2023-03-06 18:13:11 -0500

    Push all filters to url, clean up

--- js/tag.js
+++ js/tag.js
@@ -5,15 +5,9 @@ class Tag extends HTMLElement {
   connectedCallback() {
     const tag = this.getAttribute("value");
 
-    this.eventName = "tag-button-selected-tag";
-
-    if (this.hasAttribute("data-event")) {
-      this.eventName = this.getAttribute("data-event");
-    }
-
     this.addEventListener("click", () => {
       this.dispatchEvent(
-        new CustomEvent(this.eventName, {
+        new CustomEvent("tag-button-selected-tag", {
           bubbles: true,
           composed: true,
           detail: {
