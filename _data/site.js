b828bee0051ea7363b9c5a2a0ba8ae17e93f07dd
Katy DeCorah <katydecorah@gmail.com>
2023-03-06 18:58:21 -0500

    Add Sentry

---
+++ _data/site.js
@@ -0,0 +1,21 @@
+const childProcess = require("child_process");
+const process = require("process");
+
+// https://www.aleksandrhovhannisyan.com/blog/eleventy-build-info/
+const hash = childProcess
+  .execSync("git rev-parse --short HEAD")
+  .toString()
+  .trim();
+
+// eslint-disable-next-line no-undef
+module.exports = {
+  title: "Font Library",
+  url: "https://katydecorah.com",
+  baseurl: "/font-library",
+  description: "An open source project to tag and organize Google Fonts.",
+  github: "https://github.com/katydecorah/font-library",
+  contributing:
+    "https://github.com/katydecorah/font-library/blob/gh-pages/CONTRIBUTING.md",
+  env: process.env.ELEVENTY_ENV,
+  hash,
+};
