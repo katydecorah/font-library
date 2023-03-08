09cb3cd319fdd4f64a04e974277be37f04b9102a
Katy DeCorah <katydecorah@gmail.com>
2023-03-07 17:40:21 -0800

    Library action: auto-add tags, format with Prettier (#584)
    * Add auto-updating to library
    
    * remove json-stringify-pretty-compact
    
    * check style
    
    * Update update-library.yml
    
    * Update update-library.mjs
    
    * Updated local library with small caps tag; 📝 Updated generated data
    
    * Update update-library.yml
    
    * Clean up
    
    ---------
    
    Co-authored-by: GitHub Action <action@github.com>

--- .github/actions/update-library.mjs
+++ .github/actions/update-library.mjs
@@ -1,7 +1,7 @@
 import { exportVariable, setFailed, info } from "@actions/core";
 import fetch from "node-fetch";
 import { readFileSync, writeFileSync } from "fs";
-import stringify from "json-stringify-pretty-compact";
+import { execSync } from "child_process";
 
 async function library() {
   try {
@@ -9,26 +9,32 @@ async function library() {
       setFailed("GoogleToken is not set.");
       return;
     }
+    let commitMessage = [];
     const response = await fetch(
       `https://www.googleapis.com/webfonts/v1/webfonts?key=${process.env.GoogleToken}`
     );
-    const library = await response.json();
+    const libraryApi = await response.json();
     // build list of family names in Google Fonts API
-    const remoteFonts = library.items.map(({ family }) => family);
+    const fontsApi = libraryApi.items.map(({ family }) => family);
 
     // get list of families in font library
-    let local = JSON.parse(readFileSync("families.json", "utf-8"));
-    const localFonts = local.map((font) => font.family);
-    // get difference between remote and local libraries
-    const familiesToAdd = remoteFonts.filter((x) => !localFonts.includes(x));
-    // get difference between local and remote library
-    const familiesToRemove = localFonts.filter((x) => !remoteFonts.includes(x));
+    let libraryLocal = JSON.parse(readFileSync("families.json", "utf-8"));
 
-    const commitMessage = [];
+    let updateLocal = autoAddToLocal(libraryLocal);
+    libraryLocal = updateLocal.local;
+    if (updateLocal.commitMessage) {
+      commitMessage.push(updateLocal.commitMessage);
+    }
+
+    const fontsLocal = libraryLocal.map((font) => font.family);
+    // get difference between remote and local libraries
+    const familiesToAdd = fontsApi.filter((x) => !fontsLocal.includes(x));
+    // get difference between local and remote library
+    const familiesToRemove = fontsLocal.filter((x) => !fontsApi.includes(x));
 
     const { generatedMetadata, generatedFamilies } = combineLibraries(
-      library.items,
-      local
+      libraryApi.items,
+      libraryLocal
     );
     const localGeneratedData = readFileSync("js/data.js", "utf-8");
 
@@ -39,7 +45,8 @@ async function library() {
     if (
       !hasFamiliesToAdd &&
       !hasFamiliesToRemove &&
-      !hasGeneratedDataToUpdate
+      !hasGeneratedDataToUpdate &&
+      !updateLocal.updatedLocalLibrary
     ) {
       exportVariable("UpdatedLibrary", false);
       info("Nothing to update.");
@@ -47,7 +54,9 @@ async function library() {
     }
 
     if (hasFamiliesToAdd) {
-      familiesToAdd.map((font) => local.push({ family: font, tags: [] }));
+      familiesToAdd.map((font) =>
+        libraryLocal.push({ family: font, tags: [] })
+      );
       const added = `➕ Added: ${familiesToAdd.join(", ")}`;
       commitMessage.push(added);
       info(added);
@@ -55,7 +64,9 @@ async function library() {
     }
 
     if (hasFamiliesToRemove) {
-      local = local.filter((f) => !familiesToRemove.includes(f.family));
+      libraryLocal = libraryLocal.filter(
+        (f) => !familiesToRemove.includes(f.family)
+      );
       const removed = `✂️ Removed: ${familiesToRemove.join(", ")}`;
       commitMessage.push(removed);
       info(removed);
@@ -75,15 +86,16 @@ async function library() {
       exportVariable("UpdatedLibrary", true);
     }
 
-    if (hasFamiliesToAdd || hasFamiliesToRemove) {
+    if (hasFamiliesToAdd || hasFamiliesToRemove || updateLocal.commitMessage) {
       writeFileSync(
         "families.json",
-        stringify(
-          local.sort((a, b) => (a.family > b.family ? 1 : -1)),
-          { maxLength: 200 }
+        JSON.stringify(
+          libraryLocal.sort((a, b) => (a.family > b.family ? 1 : -1))
         ),
         "utf-8"
       );
+      // run prettier CLI on families.json
+      execSync("npx prettier --write families.json");
     }
 
     exportVariable("LibraryCommitMessage", commitMessage.join("; "));
@@ -94,51 +106,29 @@ async function library() {
 
 function combineLibraries(remoteFonts, local) {
   const combineLibrary = [];
-
   for (const [
     index,
     { family, variants, subsets, lastModified, category },
   ] of remoteFonts.entries()) {
     const localFont = local.find((f) => f.family === family);
-
-    // Check for main variants
-    const hasItalic = variants.includes("italic");
-    const hasBold =
-      variants.includes("500") ||
-      variants.includes("600") ||
-      variants.includes("700") ||
-      variants.includes("800") ||
-      variants.includes("900");
-    const hasRegular = variants.includes("regular") || variants.includes("400");
-    const fullVariant = hasBold && hasRegular && hasItalic;
-
     combineLibrary.push({
       family,
       slug: family.replace(/ /g, "+"),
       variants,
-      hasItalic,
-      hasBold,
-      hasRegular,
-      fullVariant,
       subsets,
       lastModified,
       category,
       tags: localFont ? localFont.tags : [],
-      count: localFont ? localFont.tags.length : 0, // number of tags
       lineNumber: index + 2,
     });
   }
 
-  const tagArr = combineLibrary.map((f) => f.tags).flat();
-  const variantArr = combineLibrary.map((f) => f.variants).flat();
-  const subsetArr = combineLibrary.map((f) => f.subsets).flat();
-  const categoryArr = combineLibrary.map((f) => f.category);
   return {
     generatedMetadata: {
-      tags: [...new Set(tagArr)].sort(),
-      categories: [...new Set(categoryArr)].sort(),
-      subsets: [...new Set(subsetArr)].sort(),
-      variants: [...new Set(variantArr)].sort(),
+      tags: getUnique(combineLibrary, "tags"),
+      categories: getUnique(combineLibrary, "category"),
+      subsets: getUnique(combineLibrary, "subsets"),
+      variants: getUnique(combineLibrary, "variants"),
     },
     generatedFamilies: `const generatedData=${JSON.stringify(
       combineLibrary.sort((a, b) => (a.family > b.family ? 1 : -1))
@@ -147,3 +137,26 @@ function combineLibraries(remoteFonts, local) {
 }
 
 export default library();
+
+function getUnique(arr, key) {
+  const values = arr.map((f) => f[key]).flat();
+  return [...new Set(values)].sort();
+}
+
+function autoAddToLocal(local) {
+  let updatedLocalLibrary = false;
+  // if family name has "SC" and does not have "small caps" tag, add it
+  local.forEach((font) => {
+    if (font.family.includes("SC") && !font.tags.includes("small caps")) {
+      font.tags.push("small caps");
+      updatedLocalLibrary = true;
+    }
+  });
+
+  return {
+    local,
+    commitMessage: updatedLocalLibrary
+      ? "Updated local library with small caps tag"
+      : undefined,
+  };
+}
