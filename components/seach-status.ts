8ed6c8b504ccb62f290d33eebe3e8137155fc956
Katy DeCorah <katydecorah@gmail.com>
2023-03-11 18:06:24 -0500

    Update seach-status.ts

--- components/seach-status.ts
+++ components/seach-status.ts
@@ -8,7 +8,8 @@ const iconClose = `<svg
     stroke-width="2"
     stroke-linecap="round"
     stroke-linejoin="round"
-    class="feather feather-x"
+    aria-hidden="true"
+    role="img"
   >
     <line x1="18" y1="6" x2="6" y2="18"></line>
     <line x1="6" y1="6" x2="18" y2="18"></line>
