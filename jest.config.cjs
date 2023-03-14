47b347ce4fc9bee3060165f1686d2bc56aa7d19b
Katy DeCorah <katydecorah@gmail.com>
2023-03-14 19:53:51 -0400

    Clean up

--- jest.config.cjs
+++ jest.config.cjs
@@ -4,4 +4,12 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "jsdom",
   clearMocks: true,
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 97,
+      lines: 100,
+      statements: 99,
+    },
+  },
 };
