38275a89e01c1071caafc0f3df9e5e80384159a5
Katy DeCorah <katydecorah@gmail.com>
2023-03-27 08:41:52 -0400

    Tags: icons, slab

--- test/main-app.test.ts
+++ test/main-app.test.ts
@@ -201,7 +201,7 @@ describe("MainApp", () => {
     expect(searchStatus).toMatchInlineSnapshot(`
       <search-status
         class="search-status"
-        results-length="474"
+        results-length="472"
         selected-category=""
         selected-search=""
         selected-subset=""
@@ -210,7 +210,7 @@ describe("MainApp", () => {
         selected-variant=""
       >
         <div>
-          Found 474 fonts: 
+          Found 472 fonts: 
         </div>
         
 
