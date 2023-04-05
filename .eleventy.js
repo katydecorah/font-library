b81891399188f16e9a20f08c5f50a809fd12aa49
Katy DeCorah <katydecorah@gmail.com>
2023-04-05 18:31:26 -0400

    Add favicon

--- .eleventy.js
+++ .eleventy.js
@@ -7,6 +7,7 @@ module.exports = function (eleventyConfig) {
   });
 
   eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("favicon.ico");
 
   eleventyConfig.on("afterBuild", () => {
     return esbuild.build({
