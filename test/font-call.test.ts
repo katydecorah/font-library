c9bcd48f956f5f3d4777e2c152d1a28dd0113f64
Katy DeCorah <katydecorah@gmail.com>
2024-04-21 18:51:49 -0400

    Update font-call.test.ts

--- test/font-call.test.ts
+++ test/font-call.test.ts
@@ -1,4 +1,7 @@
-import { fontCallVariant } from "../components/font-call";
+import {
+  fontCallVariant,
+  fontCallSelectedVariant,
+} from "../components/font-call";
 
 describe("fontCallVariant", () => {
   it("should return weight variant when first variant is a number", () => {
@@ -17,5 +20,27 @@ describe("fontCallVariant", () => {
     const variants = ["regular", "bold"];
     const result = fontCallVariant(variants);
     expect(result).toBe("");
+  });
+});
+
+describe("fontCallSelectedVariant", () => {
+  it('should return ":ital@1" when selectedVariant is "italic"', () => {
+    const result = fontCallSelectedVariant("italic");
+    expect(result).toBe(":ital@1");
+  });
+
+  it('should return ":ital,wght@1,400" when selectedVariant is "italic400"', () => {
+    const result = fontCallSelectedVariant("italic400");
+    expect(result).toBe(":ital,wght@1,400");
+  });
+
+  it('should return ":wght@400" when selectedVariant is "400"', () => {
+    const result = fontCallSelectedVariant("400");
+    expect(result).toBe(":wght@400");
+  });
+
+  it('should return ":" when selectedVariant does not include "italic" or a number', () => {
+    const result = fontCallSelectedVariant("regular");
+    expect(result).toBe(":");
   });
 });
