f0c6a32a61445498d155497f09933be2839210c6
Katy DeCorah <katydecorah@gmail.com>
2024-04-21 18:45:01 -0400

    Update font-call.ts

--- components/font-call.ts
+++ components/font-call.ts
@@ -1,6 +1,32 @@
 import { GeneratedData } from "./main-app";
 import rtlSubsets from "../data/rtl.json";
 
+export default function fontCall({
+  variants,
+  slug,
+  selectedVariant,
+  previewName,
+}: {
+  variants: GeneratedData[number]["variants"];
+  slug: string;
+  selectedVariant: string;
+  previewName: string;
+}): string {
+  let fontCallString = slug;
+
+  if (selectedVariant && selectedVariant !== "regular") {
+    fontCallString += fontCallSelectedVariant(selectedVariant);
+  }
+  // if font doesn't have regular variant, add subset to font call
+  if (!selectedVariant && !variants.includes("regular")) {
+    fontCallString += fontCallVariant(variants);
+  }
+
+  fontCallString += `&text=${encodeURIComponent(previewName)}&display=swap`;
+
+  return `https://fonts.googleapis.com/css2?family=${fontCallString}`;
+}
+
 export function fontCallVariant(
   variants: GeneratedData[number]["variants"],
 ): string {
@@ -14,7 +40,7 @@ export function fontCallVariant(
   return "";
 }
 
-function fontCallSelectedVariant(selectedVariant: string): string {
+export function fontCallSelectedVariant(selectedVariant: string): string {
   const hasItalic = selectedVariant.includes("italic");
   const variantNumber = selectedVariant.match(/\d+/g); // get number from selectedVariant
 
@@ -54,30 +80,4 @@ export function familyStyle({
     style += `font-weight: ${variantNumber[0]};`;
   }
   return style;
-}
-
-export default function fontCall({
-  variants,
-  slug,
-  selectedVariant,
-  previewName,
-}: {
-  variants: GeneratedData[number]["variants"];
-  slug: string;
-  selectedVariant: string;
-  previewName: string;
-}): string {
-  let fontCallString = slug;
-
-  if (selectedVariant && selectedVariant !== "regular") {
-    fontCallString += fontCallSelectedVariant(selectedVariant);
-  }
-  // if font doesn't have regular variant, add subset to font call
-  if (!selectedVariant && !variants.includes("regular")) {
-    fontCallString += fontCallVariant(variants);
-  }
-
-  fontCallString += `&text=${encodeURIComponent(previewName)}&display=swap`;
-
-  return `https://fonts.googleapis.com/css2?family=${fontCallString}`;
 }
