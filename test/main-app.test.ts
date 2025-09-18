69bc2186dfaa85b3ec80b49c552acf88b52a7b55
GitHub Action <action@github.com>
2025-09-18 05:36:38 +0000

    Review auto-tag
    Updated local library with expanded tag; 📝 Updated generated data

--- test/main-app.test.ts
+++ test/main-app.test.ts
@@ -199,11 +199,11 @@ describe("MainApp", () => {
     const searchStatus = document.querySelector("search-status");
     expect(searchStatus).toMatchInlineSnapshot(`
       <search-status
-        results-length="571"
+        results-length="569"
         selected-tag="need tags"
       >
         <div>
-          Found 571 fonts: 
+          Found 569 fonts: 
         </div>
         
 
